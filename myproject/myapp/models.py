5bcd153764d17c741214b995422be5f6be08a590
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-08-09 16:50:21 -0700

    Create models

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -4,3 +4,8 @@ from datetime import datetime
 # Create your models here.
 
 class Picture(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_on = models.DateTimeField(default=datetime.now)
+    image = models.ImageField(upload_to="images/%Y/%m/%d")
+    
