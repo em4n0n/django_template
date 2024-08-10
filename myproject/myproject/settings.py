a34cc6d031fa0a7209411883605a618883ff61a4
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-08-09 20:23:03 -0700

    Add media root and url for uploading images

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp'
 ]
 
 MIDDLEWARE = [
@@ -114,6 +115,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media'
 
 STATIC_URL = 'static/'
 
