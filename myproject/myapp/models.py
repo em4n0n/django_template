eb22a9cb7d9e30c41447b919615c4a57d2112c0a
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-08-10 20:47:04 -0700

    Delete datetime module

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,13 +1,10 @@
 from django.db import models
-from datetime import datetime
 
 # Create your models here.
 
 class Picture(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    created_on = models.DateTimeField(default=datetime.now)
-    image = models.ImageField(upload_to="images/%Y/%m/%d")
 
     def __str__(self):
         return self.title
@@ -15,8 +12,15 @@ class Picture(models.Model):
 obj = Picture(title = "Remmington", description = "cat")
 obj.save()
 
+# to modify an object
 # obj = Picture.object.get(id=1)
 # obj.title = "Picture"
 # obj.save()
+
+# Picture.object.all()
+
+# to delete an object
+# obj = Picture.object.get(id=1)
+# obj.delete()
 
 # Picture.object.all()
