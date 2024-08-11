5f95fd455934e18f1d4ae10bde62cb4e3fbbb9f2
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-08-11 16:26:10 -0700

    Add a HttpResponse

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -4,3 +4,11 @@ from django.template import loader
 
 # Create your views here.
 def index(request):
+    path = request.path
+    method = request.method
+    content='''
+<center><h2>Testing Django Request Response Objects</h2>
+<p>Request path: {}</p>
+<p>Request Method :{}</p></center>
+'''.format(path, method)
+    return HttpResponse(content)
