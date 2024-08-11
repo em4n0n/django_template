79da30a6738dce0c86aef1426b30025bf500b357
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-08-11 16:04:59 -0700

    Define a index function and inherit request

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello World!")
+    return HttpResponse('<h1> Welcome! </h1>')
+
+def index(request):
